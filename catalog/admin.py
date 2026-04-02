a0e67d2203a52e022024b050fd0deda99b104444
Gabyyy123 <gabrillojhonloyd@gmail.com>
2026-04-03 02:18:52 +0800

    Registered all models in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile, Category, Book, BorrowRecord, Computer, MeetingRoomSchedule
 
-# Register your models here.
+# Registering your models so they appear in the Django Admin panel
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(BorrowRecord)
+admin.site.register(Computer)
+admin.site.register(MeetingRoomSchedule)
