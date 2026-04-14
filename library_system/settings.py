6503d06fa2a26a7aa066720d1afae8cc2a33d782
Gabyyy123 <gabrillojhonloyd@gmail.com>
2026-04-15 06:31:27 +0800

    Fixed auto-refresh bug and integrated Cloudinary

--- library_system/settings.py
+++ library_system/settings.py
@@ -1,6 +1,7 @@
 import os  # This MUST be at the top
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 
@@ -22,6 +23,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load the .env file BEFORE anything else happens!
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
